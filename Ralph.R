6796a1d7d8f4f1483e83b9c8853282961dcba311
RapRap9 <ralph.rafael@obf.ateneo.edu>
2021-07-02 11:00:34 +0800

    I created print lines on a new R file.

---
+++ Ralph.R
@@ -0,0 +1,4 @@
+print ("Hello! My name is Ralph Anthony Rafael.")
+print ("I am 19 years old and I really like coding even if I am still a beginner.")
+print ("I hope to learn more about R as we go along this subject!")
+
